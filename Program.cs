4efef39caa2af508f14d3b25b70815f181f090fc
s66829020@gmail.com <s66829020@gmail.com>
2023-07-27 00:32:51 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace AP_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+
+        public static void runApp()
+        {
+            string[] students10A, students10B;
+
+            students10A = new string[10] { "Rahul", "Suman", "Mukesh", "Afzal", "Ramesh", "Geeta", "Jason", "Robert", "Gopal", "Meera" };
+            students10B = new string[10] { "Pinky", "Rakesh", "Rafi", "Mumtaz", "Derek", "Sukhwinder", "Gopi", "Tulsi", "Chandrika", "Ann" };
+
+            string[] subjects = new string[6];
+            subjects[0] = "Physics";
+            subjects[1] = "Chemistry";
+            subjects[2] = "Biology";
+            subjects[3] = "Calculus";
+            subjects[4] = "Computer Science";
+            subjects[5] = "Algebra";
+
+            int[] marks = new int[6];
+            marks[0] = 67;
+            marks[1] = 90;
+            marks[2] = 80;
+            marks[3] = 55;
+            marks[4] = 71;
+            marks[5] = 92;
+
+            Console.WriteLine("Students of Class 10A:");
+            foreach (string s in students10A)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine("");
+
+            Console.WriteLine("Students of Class 10B:");
+            foreach (string s in students10B)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine("");
+
+            Console.WriteLine("Marks of Kamal:");
+            int total = 0;
+            for (int i = 0; i < 6; i++)
+            {
+                total += marks[i];
+                Console.WriteLine(subjects[i] + " = " + marks[i]);
+            }
+            Console.WriteLine("TOTAL = " + total + "/600 = " + (total * 100 / 600) + " percent");
+        }
+    }
+}
+
+
